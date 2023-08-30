0b4eafcab53b8d93ba9aa9b2f660b5549a22fa90
tekhunt <chizzydmec@gmail.com>
2023-08-30 09:06:11 +0100

    setup project structure

---
+++ Makefile
@@ -0,0 +1,14 @@
+run:
+	python manage.py runserver 8005
+
+migrations:
+	python manage.py makemigrations
+
+migrate:
+	python manage.py migrate
+
+superuser:
+	python manage.py createsuperuser
+
+test:
+	python3 manage.py test
---
+++ api/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from api.models import User
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+        ]
+
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super.update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
